6be46acf839b513baf3a404fa3f3eb1fd47f93f6
vjdivyanshi-create <vj.divyanshi@gmail.com>
2026-04-19 19:16:07 +0530

    Update configuration files

--- Frontend/react/vite.config.js
+++ Frontend/react/vite.config.js
@@ -1,13 +1,9 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   const backendUrl = env.VITE_BACKEND_URL || 'http://localhost:5000'
 
   return {
-    base: "/", // ✅ ADD THIS LINE
+    base: "./", // ✅ VERY IMPORTANT FIX
 
     plugins: [react(), tailwindcss()],
     server: {
